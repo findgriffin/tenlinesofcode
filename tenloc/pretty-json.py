9e8f33a6e42c735cda91d6e24033ff17aaa737a9
David Griffin <dgriffin@arpio.io>
2023-12-12 21:58:12 -0800

    Improvements to pretty-json.py

--- tenloc/pretty-json.py
+++ tenloc/pretty-json.py
@@ -3,16 +3,21 @@ import json
 import sys
 
 
-def reformat_json(input_file):
-    with open(input_file, 'r') as in_json:
-        with open(input_file.split('.')[:-1] + '-pretty.json', 'w') as out_json:
+def pretty_json(input_filename):
+    out_filename = '.'.join(input_filename.split('.')[:-1]) + '-pretty.json'
+    with open(input_filename, 'r') as in_json:
+        with open(out_filename, 'w') as out_json:
             json.dump(json.load(in_json), out_json, indent=2)
+            print(f'Wrote JSON to: {out_filename}')
 
 
 if __name__ == '__main__':
     for file_name in sys.argv[1:]:
         if file_name.endswith('.json'):
-            reformat_json(file_name)
+            try:
+                pretty_json(file_name)
+            except json.decoder.JSONDecodeError as err:
+                print(f'Could not decode {file_name}... {err}')
         else:
             print(f'Skipping non-JSON file {file_name}')
 
